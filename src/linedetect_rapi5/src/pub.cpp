4e92c9b11af84f0e0fe0698cca0eb67edb0dcb31
bbanggang <hungue6559@naver.com>
2026-02-06 19:39:30 +0900

    linedetect

--- src/linedetect_rapi5/src/pub.cpp
+++ src/linedetect_rapi5/src/pub.cpp
@@ -2,45 +2,47 @@
 #include "sensor_msgs/msg/compressed_image.hpp"
 #include "cv_bridge/cv_bridge.hpp"
 #include "opencv2/opencv.hpp"
-#include <memory>
 #include <chrono>
-#include <rclcpp/executors.hpp>
 
-std::string video_path = "/home/rapi5/ros2_ws/video/simulation/5_lt_cw_100rpm_out.mp4";
+class VideoPublisher : public rclcpp::Node {
+public:
+    VideoPublisher() : Node("video_publisher_node") {
+        auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();
+        raw_image_pub_ = this->create_publisher<sensor_msgs::msg::CompressedImage>("image/raw/compressed", qos_profile);
+        
+        // 비디오 파일 열기
+        cap_.open("/home/rapi5/ros2_ws/video/simulation/7_lt_ccw_100rpm_in.mp4"); 
+        if (!cap_.isOpened()) {
+            RCLCPP_ERROR(this->get_logger(), "Could not open video file!");
+        }
 
-
-int main(int argc, char * argv[])
-{
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("video_pub");
-    // auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)); //TCP
-    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(); //UDP
-    auto mypub = node->create_publisher<sensor_msgs::msg::CompressedImage>("image/compressed", qos_profile );
-    
-    std_msgs::msg::Header hdr;
-    sensor_msgs::msg::CompressedImage::SharedPtr msg;
-    rclcpp::WallRate loop_rate(30.0);
-
-    cv::VideoCapture cap(video_path);
-    if (!cap.isOpened()) {
-        RCLCPP_ERROR(node->get_logger(), "Could not open video!");
-        rclcpp::shutdown();
-        return -1;
-    }  
-    cv::Mat frame;
-
-    while(rclcpp::ok())
-    {
-        cap >> frame;
-        if (frame.empty()) { RCLCPP_ERROR(node->get_logger(), "frame empty"); break;}
-
-        hdr.stamp = node->get_clock()->now();
-        msg = cv_bridge::CvImage(hdr, "bgr8", frame).toCompressedImageMsg();
-        mypub->publish(*msg);
-        loop_rate.sleep();
-        rclcpp::spin_some(node);
+        // 30FPS 주기로 영상만 전송
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(33), std::bind(&VideoPublisher::timer_callback, this));
     }
-    cap.release();
+
+private:
+    void timer_callback() {
+        cv::Mat frame;
+        cap_ >> frame;
+
+        if (frame.empty()) {
+            cap_.set(cv::CAP_PROP_POS_FRAMES, 0); // 무한 반복 재생
+            cap_ >> frame;
+        }
+
+        // 압축 영상 메시지 생성 및 전송 
+        auto msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", frame).toCompressedImageMsg();
+        raw_image_pub_->publish(*msg);
+    }
+
+    cv::VideoCapture cap_;
+    rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr raw_image_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char** argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<VideoPublisher>());
     rclcpp::shutdown();
     return 0;
 }
